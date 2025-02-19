1dee9e1b29289e6d840946a23ffa7926943d25df
asxw <58903706+asxw@users.noreply.github.com>
2025-02-19 12:34:11 +0630

    Delete routes directory

--- routes/food.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', async function(req, res) {
-    res.status(200).json({msg: 'This is Cors-enabled for all origins!'});
-});
-
-module.exports = router;
